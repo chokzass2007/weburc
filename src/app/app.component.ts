1231ff3291a6b6f387e6364df60383b3e66dc90f
teeradaj jaemkrajang <teeradaj.j@urcricemail.com>
2024-07-05 09:40:58 +0700

    โชคบอกโค้ดไม่มา

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
   })
   export class AppComponent {
-    // คอมโพเนนต์ของคุณที่นี่
+    showError: boolean = false;
+  
+    constructor(private router: Router) {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          this.showError = event.urlAfterRedirects === '/';
+        }
+      });
+    }
   }
