ece2c69b89d68884dd21542549a6fdd9252f5b54
teeradaj jaemkrajang <teeradaj.j@urcricemail.com>
2024-07-05 10:40:23 +0700

    ย้าย app-routing.modele.ts ไป app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChillerComponent } from './chiller/chiller.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'urcweb/home', pathMatch: 'full' },
+  { path: 'Chiller', component: ChillerComponent },
+  // เพิ่ม Route อื่น ๆ ที่นี่
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
