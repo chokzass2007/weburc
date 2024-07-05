bc6bbe21af6ce7a7f30fceabf2ab16da2ce8e62e
teeradaj jaemkrajang <teeradaj.j@urcricemail.com>
2024-07-05 11:20:10 +0700

    ลบenvironment

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.prod.ts
-
-export const environment = {
-    production: true,  // ใช้เพื่อระบุว่าเป็นสภาพแวดล้อมการผลิต
-    apiUrl: 'https://api.example.com'  // URL ของ API สำหรับสภาพแวดล้อมการผลิต
-  };
-  
